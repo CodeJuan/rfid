3fc58b0afc3ba60733757b6d212d905c2fd55e49
codejuan <xh@decbug.com>
2016-01-05 20:50:21 +0800

    add models & add products to settings

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class RFID(models.Model):
+    RFID = models.CharField(max_length=12)
+    AntennaID = models.CharField(max_length=20)
+
+class Proxy(models.Model):
+    ShopID = models.CharField(max_length=20)
+    ProxyID = models.CharField(max_length=20)
