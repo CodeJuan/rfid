c3e5df233c6d79676d7377906b3de03f13f046c1
Jackon <jiekunyang@gmail.com>
2016-01-07 01:15:01 +0800

    bug fix. initial data for shops and equipments

--- backup.sh
+++ backup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-mkdir -p shops/fixtures/
-mkdir -p equipments/fixtures/
-
-python manage.py dumpdata --indent=4 shops > shops/fixtures/initial_data.json
-python manage.py dumpdata --indent=4 equipments > equipments/fixtures/initial_data.json
-
 rm -rf init_data
 cp -R media init_data
+
+python manage.py dumpdata --natural-foreign --exclude auth.permission --exclude contenttypes --indent 4 > init_data/initial_data.json
--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ python manage.py createsuperuser --username=jackon --email=jiekunyang@gmail.com
 
 python manage.py check_permissions  # required by userena
 
+python manage.py loaddata init_data/initial_data.json
+
 rm -rf media
 cp -R init_data media
