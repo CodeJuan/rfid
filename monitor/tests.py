70f113837910d44f1ebb20458335c5da48d67254
codejuan <xh@decbug.com>
2016-01-06 22:32:01 +0800

    test api of report

--- monitor/tests.py
+++ monitor/tests.py
@@ -1,14 +1,25 @@
 import time
+from django.test import TestCase
+import requests
+import urllib2
+import json
+from json import JSONEncoder
 import redis
 
 def test():
     r = redis.Redis(host='192.168.1.245', port=6379, db=0)
-    r.set('Hi', 'hello')
-    r.expire('Hi',5)
-    time.sleep(1)
-    print r.get('Hi')
+    print r.get('22222222')
     time.sleep(8)
-    print r.get('Hi')
+    print r.get('22222222')
+
+def testPostReport():
+    print 'aaaa'
+    headers = {'content-type': 'application/json'}
+    payload = {"AntennaID": "55555555", "RFID": "22222222"}
+    r = requests.post('http://localhost:8000/api/v1/report/', data=json.dumps(payload), headers=headers)
+    print r.status_code
+    print r.json()
 
 if __name__ == '__main__':
+    testPostReport()
     test()
