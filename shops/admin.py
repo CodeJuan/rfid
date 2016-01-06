3e829d5299823c776175707959e16562dc81e31b
Jackon <jiekunyang@gmail.com>
2016-01-07 00:27:05 +0800

    bug fix. show shop admin in admin page

--- shops/admin.py
+++ shops/admin.py
@@ -4,7 +4,7 @@ from shops.models import Shop
 
 
 class ShopAdmin(admin.ModelAdmin):
-    list_display = ('name', 'addr', 'addr', 'contact', 'phone', 'status')
+    list_display = ('name', 'addr', 'admin', 'contact', 'phone', 'status')
 
 
 admin.site.register(Shop, ShopAdmin)
