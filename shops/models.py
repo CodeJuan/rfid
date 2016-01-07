75a1f5b4af72b0b56cce64e37244829a953561bd
Jackon <jiekunyang@gmail.com>
2016-01-07 14:58:58 +0800

    shops model rename

--- shops/models.py
+++ shops/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 
 class Shop(models.Model):
-    name = models.CharField(u'店铺名称', max_length=100)
+    name = models.CharField(u'门店名称', max_length=100)
     addr = models.CharField(u'地址', max_length=100)
-    admin = models.ForeignKey(User, verbose_name=u'管理员')
+    owner = models.ForeignKey(User, verbose_name=u'负责人')
     contact = models.CharField(u'联系人', max_length=100)
     phone = models.CharField(u'电话', max_length=100)
     status = models.IntegerField(u'状态', choices=(
@@ -18,5 +18,5 @@ class Shop(models.Model):
         return u'%s(%s)' % (self.name, self.addr)
 
     class Meta:
-        verbose_name = u'店铺'
+        verbose_name = u'门店'
         verbose_name_plural = verbose_name
