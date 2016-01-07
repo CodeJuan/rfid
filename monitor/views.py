db4f544cb44b29d44b4f64dc5c4829cef3bf15b6
CodeJuan <CodeJuan@users.noreply.github.com>
2016-01-07 23:28:09 +0800

    redis config on daocloud

--- monitor/views.py
+++ monitor/views.py
@@ -7,6 +7,9 @@ from monitor.models import Report
 from monitor.serializers import ReportSerializer
 import redis
 
+redis = Redis(host=os.environ['REDIS_PORT_6379_TCP_ADDR'],
+              port=os.environ['REDIS_PORT_6379_TCP_PORT'],
+              password=os.environ.get('REDIS_PASSWORD'))
 
 @api_view(['POST'])
 def post_report(request):
@@ -16,7 +19,7 @@ def post_report(request):
         data = {'RFID': rfid, 'AntennaID': request.data.get('AntennaID')}
         serializer = ReportSerializer(data=data)
         if serializer.is_valid():
-            r = redis.Redis(host='192.168.1.245', port=6379, db=0)
-            r.set(rfid, '1')
-            r.expire(rfid, 15)
+            #r = redis.Redis(host='192.168.1.245', port=6379, db=0)
+            redis.set(rfid, '1')
+            redis.expire(rfid, 15)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
