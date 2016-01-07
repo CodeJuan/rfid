5949486439c1ca4cf716c39cd7d5dbea9265c45d
Jackon <jiekunyang@gmail.com>
2016-01-07 17:11:19 +0800

    sidebar with django variables

--- base/context_processors.py
+++ base/context_processors.py
@@ -4,7 +4,15 @@
 def site_info(request):
     return {'brand_logo': 'DjangoStart',  # show in navbar
             'nav_list': (
-                ('/', 'Home'),
-                ('/admin', 'Admin'),
+                ('#', u'入库'),
+                ('#', u'RFID 监控'),
+                ('#', u'RFID 列表'),
+                ('#', u'盘点'),
+                ('#', u'出库'),
+                ('#', u'上柜 下柜'),
+                ('#', u'门店管理'),
+                ('#', u'部门和员工'),
+                ('#', u'角色和权限'),
+                ('#', u'设置'),
                 ),
             }
