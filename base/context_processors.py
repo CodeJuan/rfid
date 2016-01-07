ed3ada5be7c1a1d74b69a2d066836d795adcc959
Jackon <jiekunyang@gmail.com>
2016-01-07 18:28:11 +0800

    show admin in navbar and remove settings

--- base/context_processors.py
+++ base/context_processors.py
@@ -5,15 +5,13 @@ from django.core.urlresolvers import reverse
 def site_info(request):
     return {'brand_logo': 'DjangoStart',  # show in navbar
             'nav_list': (
-                ('#', u'入库'),
-                ('#', u'RFID 监控'),
+                ('/', u'RFID 监控'),
                 ('#', u'RFID 列表'),
+                ('#', u'入库'),
                 ('#', u'盘点'),
                 ('#', u'出库'),
                 ('#', u'上柜 下柜'),
-                (reverse('shops_home'), u'门店管理'),
-                ('#', u'部门和员工'),
-                ('#', u'角色和权限'),
-                ('#', u'设置'),
+                (reverse('shops_home'), u'门店'),
+                ('/admin', u'管理台'),
                 ),
             }
